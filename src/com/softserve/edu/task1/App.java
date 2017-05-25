6fec808aa8128f27a1f32ebe7c04f6e15337eac0
Crutovel <crutovel@gmail.com>
2017-05-25 14:18:58 +0300

    formatting&remove useless exception update

--- src/com/softserve/edu/task1/App.java
+++ src/com/softserve/edu/task1/App.java
@@ -3,41 +3,32 @@ package com.softserve.edu.task1;
 public class App {
 
 	public static void main(String[] args) {
-		
-		int w=0,h=0;
-		
-		try {
-			w = Integer.valueOf(args[0]);
-			h = Integer.valueOf(args[1]);
-		} catch (NumberFormatException e) {
-			System.out.println("Wrong number format, need Integer value");
-			System.exit(0);
-
-		} catch (ArrayIndexOutOfBoundsException e) {
+		int w = 0, h = 0;
+		if (args.length != 2) {
 			System.out.println("Enter more parametrs");
 			System.exit(0);
+		} else {
+			try {
+				w = Integer.valueOf(args[0]);
+				h = Integer.valueOf(args[1]);
+			} catch (NumberFormatException e) {
+				System.out.println("Wrong number format, need Integer value");
+				System.exit(0);
+			}
+			if (w <= 0 || h <= 0) {
+				System.out.println("Parameters must be greater than zero");
+				System.exit(0);
+			}
+			int n = (w * 2 * h) + (h / 2) - 1;
+			if (n < 0) {
+				System.out.println("Parameters too big");
+				System.exit(0);
+			}
+			System.out.print(makeChessBoard(w, h, n));
 		}
-		
-		if(w<=0||h<=0){
-			System.out.println("Parameters must be greater than zero");
-			System.exit(0);
-		}
-		
-		int n = (w * 2 * h) + (h / 2) - 1;
-		
-		if(n<0){
-			System.out.println("Parameters too big");
-			System.exit(0);
-		}
-		
-
-		
-		System.out.print(makeChessBoard(w, h, n));
-
 	}
 
 	public static char[] makeChessBoard(int w, int h, int n) {
-
 		char[] array = new char[n];
 		w *= 2;
 		int m = 0;
@@ -58,9 +49,7 @@ public class App {
 					m++;
 				}
 			}
-
 		}
-
 		return array;
 	}
 
