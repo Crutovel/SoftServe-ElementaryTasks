fcaeecacde98c8a3ac463ce9609eb80e00f726c4
Crutovel <crutovel@gmail.com>
2017-05-25 14:21:21 +0300

    formatting&remove useless exception update

--- src/com/softserve/edu/task7/App.java
+++ src/com/softserve/edu/task7/App.java
@@ -3,24 +3,22 @@ package com.softserve.edu.task7;
 public class App {
 
 	public static void main(String[] args) {
-
 		int n = 0;
-
+		if (args.length != 1) {
+			System.out.println("Need one arg");
+			System.exit(0);
+		}
 		try {
 			n = Integer.valueOf(args[0]);
 		} catch (NumberFormatException e) {
 			System.out.println("Wrong number format, need Integer value");
 			System.exit(0);
 
-		} catch (ArrayIndexOutOfBoundsException e) {
-			System.out.println("Enter more parametrs");
-			System.exit(0);
 		}
 		if (n <= 0) {
 			System.out.println("Parameters must be greater than zero");
 			System.exit(0);
 		}
-
 		double max = (double) Math.sqrt(n);
 		int trueMax = (int) max;
 		if (max == (double) trueMax) {
