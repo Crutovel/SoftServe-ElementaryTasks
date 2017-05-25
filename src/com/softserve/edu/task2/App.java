ca001b1ff51756e67e4626124633394e34cac61f
Crutovel <crutovel@gmail.com>
2017-05-25 14:14:23 +0300

    formatting fixes

--- src/com/softserve/edu/task2/App.java
+++ src/com/softserve/edu/task2/App.java
@@ -4,42 +4,42 @@ import java.util.Scanner;
 
 public class App {
 
-	public static double parseDouble(String str){
+	public static double parseDouble(String str) {
 		str = str.replace(",", ".");
 		return Double.valueOf(str);
 	}
+
 	public static void main(String[] args) {
 		double a = 0, b = 0, c = 0, d = 0;
 		String confirm = "";
 		String number = "";
 		Scanner scanner = new Scanner(System.in);
-		
+
 		for (;;) {
-			
-			try{
-			System.out.println("Enter a: ");
-			number = scanner.next();
-			a = parseDouble(number);
-			
-			System.out.println("Enter b: ");
-			number = scanner.next();
-			b = parseDouble(number);
-		
-			System.out.println("Enter c: ");
-			number = scanner.next();
-			c = parseDouble(number);
-			
-			System.out.println("Enter d: ");
-			number = scanner.next();
-			d = parseDouble(number);
-			}
-			catch(NumberFormatException e){
+
+			try {
+				System.out.println("Enter a: ");
+				number = scanner.next();
+				a = parseDouble(number);
+
+				System.out.println("Enter b: ");
+				number = scanner.next();
+				b = parseDouble(number);
+
+				System.out.println("Enter c: ");
+				number = scanner.next();
+				c = parseDouble(number);
+
+				System.out.println("Enter d: ");
+				number = scanner.next();
+				d = parseDouble(number);
+			} catch (NumberFormatException e) {
 				scanner.close();
 				System.out.println("Wrong number format");
 				System.exit(0);
 			}
-			
-			if(a<=0||b<=0||c<=0||d<=0){
+
+			if (a <= 0 || b <= 0 || c <= 0 || d <= 0) {
 				scanner.close();
 				System.out.println("Parameters must be greater than zero");
 				System.exit(0);
