0c343d7434963d2c49a2969bc31aeef0f961a246
Crutovel <crutovel@gmail.com>
2017-05-31 15:59:56 +0300

    task 2 small fixes

--- src/com/softserve/edu/task2/App.java
+++ src/com/softserve/edu/task2/App.java
@@ -36,36 +36,36 @@ public class App {
 			} catch (NumberFormatException e) {
 				scanner.close();
 				System.out.println("Wrong number format");
-				System.exit(0);
+				System.exit(5);
 			}
 
 			if (a <= 0 || b <= 0 || c <= 0 || d <= 0) {
 				scanner.close();
 				System.out.println("Parameters must be greater than zero");
-				System.exit(0);
-			}
 
-			if ((a < c && b < d) || (a < d && b < c) || (c < a && d < b) || (c < b && d < a)) {
-				System.out.println("Possible");
 			} else {
-				System.out.println("Impossible");
-			}
 
-			System.out.println("Do you want to continue?");
-			confirm = scanner.next();
-			confirm = confirm.toLowerCase();
-			switch (confirm) {
-			case "y": {
-				break;
-			}
-			case "yes": {
-				break;
-			}
-			default: {
-				scanner.close();
-				System.out.println("bb");
-				System.exit(1);
-			}
+				if ((a < c && b < d) || (a < d && b < c) || (c < a && d < b) || (c < b && d < a)) {
+					System.out.println("Possible");
+				} else {
+					System.out.println("Impossible");
+				}
+
+				System.out.println("Do you want to continue?");
+				confirm = scanner.next();
+				confirm = confirm.toLowerCase();
+				switch (confirm) {
+				case "y": {
+					break;
+				}
+				case "yes": {
+					break;
+				}
+				default: {
+					scanner.close();
+					System.out.println("bb");
+				}
+				}
 			}
 		}
 	}
