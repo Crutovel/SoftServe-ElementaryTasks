83cd85c23f2ad50f60c7fed0ea7b030a7f3fb2f5
Crutovel <crutovel@gmail.com>
2017-06-07 17:09:59 +0300

    task 7 updated

--- src/com/softserve/edu/task7/App.java
+++ src/com/softserve/edu/task7/App.java
@@ -2,33 +2,30 @@ package com.softserve.edu.task7;
 
 public class App {
 
-	public static void main(String[] args) {
-		int n = 0;
-		if (args.length != 1) {
-			System.out.println("Need one arg");
-			System.exit(0);
-		}
-		try {
-			n = Integer.valueOf(args[0]);
-		} catch (NumberFormatException e) {
-			System.out.println("Wrong number format, need Integer value");
-			System.exit(0);
-
-		}
-		if (n <= 0) {
-			System.out.println("Parameters must be greater than zero");
-			System.exit(0);
-		}
-		double max = (double) Math.sqrt(n);
-		int trueMax = (int) max;
-		if (max == (double) trueMax) {
-			trueMax--;
-		}
-
-		for (int i = 1; i < trueMax; i++) {
-			System.out.print(i + ",");
-		}
-		System.out.print(trueMax);
-	}
-
+    public static void main(String[] args) {
+        int n = 0;
+        if (args.length != 1) {
+            System.out.println("Need one argument");
+        } else {
+            try {
+                n = Integer.valueOf(args[0]);
+            } catch (NumberFormatException e) {
+                System.out.println("Wrong number format, need Integer value");
+                System.exit(2);
+            }
+            if (n <= 0) {
+                System.out.println("Parameters must be greater than zero");
+            } else {
+                double max = (double) Math.sqrt(n);
+                int trueMax = (int) max;
+                if (max == (double) trueMax) {
+                    trueMax--;
+                }
+                for (int i = 1; i < trueMax; i++) {
+                    System.out.print(i + ",");
+                }
+                System.out.print(trueMax);
+            }
+        }
+    }
 }
