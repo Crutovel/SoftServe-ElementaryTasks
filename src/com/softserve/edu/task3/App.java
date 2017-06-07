6890b4bf6b97b78f7e8f93453723591842a497b6
Crutovel <crutovel@gmail.com>
2017-06-07 16:43:08 +0300

    task 3 updated

--- src/com/softserve/edu/task3/App.java
+++ src/com/softserve/edu/task3/App.java
@@ -7,71 +7,76 @@ import java.util.Scanner;
 
 public class App {
 
-	public static void main(String[] args) {
-		boolean main = true;
-		boolean create = true;
-		Scanner scanner = new Scanner(System.in);
-		ArrayList<Triangle> triangles = new ArrayList<Triangle>();
-		for (; main;) {
-			System.out.println("Enter - <1> to create triangle");
-			System.out.println("Enter - <2> to show all triangles");
-			System.out.println("Enter - <3> exit");
-			if (scanner.hasNextInt()) {
-				switch (Integer.valueOf(scanner.next())) {
-				case 1: {
-					for (; create;) {
-						System.out.println("Format - <name>, <side1>, <side2>, <side3>");
-						try {
-							triangles.add(Triangle.valueOf(scanner.next()));
-						} catch (IllegalArgumentException e) {
-							System.out.println("Wrong format");
-							scanner.close();
-						}
-						System.out.println("More?");
-						switch (scanner.next().toLowerCase()) {
-						case "y": {
+    public static final double PRECISION = 0.000001;
 
-						}
-						case "yes": {
-							break;
-						}
-						default: {
-							Collections.sort(triangles, new Comparator<Triangle>(){
-							     public int compare(Triangle o1, Triangle o2){
-							         if(o1.getArea() == o2.getArea())
-							             return 0;
-							         return o1.getArea() < o2.getArea() ? 1 : -1;
-							     }
-							});
-							create = false;
-							break;
-						}
-						}
-					}
-					break;
-				}
-				case 2: {
-					System.out.println("============= Triangles list: ===============");
-					int i = 1;
-					for (Triangle item : triangles) {
-						System.out.println(i + ". " + item.toString());
-						i++;
-					}
-					break;
-				}
-				case 3: {
-					main = false;
-					break;
-				}
-				default: {
-					System.out.println("srsly? pls enter 1 or 2");
-					break;
-				}
-				}
-			}
-		}
-		scanner.close();
+    public static void main(String[] args) {
+        boolean main = true;
+        boolean create = true;
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<Triangle> triangles = new ArrayList<Triangle>();
+        for (; main;) {
+            System.out.println("Enter - <1> to create triangle");
+            System.out.println("Enter - <2> to show all triangles");
+            System.out.println("Enter - <3> exit");
+            if (scanner.hasNextInt()) {
+                switch (Integer.valueOf(scanner.next())) {
+                    case 1: {
+                        while (create) {
+                            System.out.println("Format - <name>, <side1>, <side2>, <side3>");
+                            try {
+                                triangles.add(Triangle.valueOf(scanner.next()));
+                            } catch (IllegalArgumentException e) {
+                                System.out.println("Wrong format");
+                                break;
+                            }
+                            System.out.println("More(y/n)?");
+                            switch (scanner.next().toLowerCase()) {
+                                case "y": {
 
-	}
+                                }
+                                case "yes": {
+                                    break;
+                                }
+                                default: {
+                                    Collections.sort(triangles, new Comparator<Triangle>() {
+                                        public int compare(Triangle o1, Triangle o2) {
+                                            double delta = o1.getArea() - o2.getArea();
+                                            if (delta > PRECISION) {
+                                                return -1;
+                                            } else {
+                                                return delta < -PRECISION ? 1 : 0;
+                                            }
+                                        }
+                                    });
+                                    create = false;
+                                    break;
+                                }
+                            }
+                        }
+                        break;
+                    }
+                    case 2: {
+                        System.out.println("============= Triangles list: ===============");
+                        int i = 1;
+                        for (Triangle item : triangles) {
+                            System.out.println(i + ". " + item.toString());
+                            i++;
+                        }
+                        break;
+                    }
+                    case 3: {
+                        main = false;
+                        break;
+                    }
+                    default: {
+                        System.out.println("srsly? pls enter 1 or 2");
+                        break;
+                    }
+                }
+            }
+        }
+        scanner.close();
+
+    }
 
 }
--- src/com/softserve/edu/task3/Triangle.java
+++ src/com/softserve/edu/task3/Triangle.java
@@ -1,92 +1,92 @@
 package com.softserve.edu.task3;
 
 public class Triangle {
-	private String name;
-	private double sides[];
+    private String name;
+    private double sides[];
 
-	private double area;
+    private double area;
 
-	public double getArea() {
-		return area;
-	}
+    public double getArea() {
+        return area;
+    }
 
-	public Triangle() {
-		name = "null";
-		sides = new double[3];
-	}
+    public Triangle() {
+        name = "null";
+        sides = new double[3];
+    }
 
-	public Triangle(String name, double fSide, double sSide, double tSide) {
-		this();
-		this.name = name;
-		if (!triangleSidesValidator(fSide, sSide, tSide)) {
-			throw new IllegalArgumentException();
-		}
-		sides[0] = fSide;
-		sides[1] = sSide;
-		sides[2] = tSide;
-		area = calculateArea();
-	}
+    public Triangle(String name, double fSide, double sSide, double tSide) {
+        this();
+        this.name = name;
+        if (!triangleSidesValidator(fSide, sSide, tSide)) {
+            throw new IllegalArgumentException();
+        }
+        sides[0] = fSide;
+        sides[1] = sSide;
+        sides[2] = tSide;
+        area = calculateArea();
+    }
 
-	@Override
-	public String toString() {
-		return "[Triangle " + name + "]: " + String.format("%.2f", area) + " cm";
-	}
+    @Override
+    public String toString() {
+        return "[Triangle " + name + "]: " + String.format("%.2f", area) + " cm";
+    }
 
-	public static Triangle valueOf(String input) {
-		String name = "";
-		double[] trSides = new double[3];
-		String str = new String(input.toLowerCase());
-		str = deleteSymbols(str, " ");
-		str = deleteSymbols(str, "\t");
-		int index = 0;
-		int commaChecker;
-		for (commaChecker = 0; index != -1; commaChecker++) {
-			index = str.indexOf(",");
-			if (commaChecker == 0) {
-				if (index == 0) {
-					throw new IllegalArgumentException();
-				} else {
-					name = str.substring(0, index);
-				}
-			}
-			if (commaChecker == 1) {
-				trSides[0] = Double.valueOf(str.substring(0, index));
-			}
-			if (commaChecker == 2) {
-				trSides[1] = Double.valueOf(str.substring(0, index));
-				trSides[2] = Double.valueOf(str.substring(index + 1, str.length()));
-			}
-			str = str.substring(index + 1, str.length());
-		}
-		if (commaChecker != 4) {
-			throw new IllegalArgumentException();
-		}
-		return new Triangle(name, trSides[0], trSides[1], trSides[2]);
-	}
+    public static Triangle valueOf(String input) {
+        String name = "";
+        double[] trSides = new double[3];
+        String str = new String(input.toLowerCase());
+        str = deleteSymbols(str, " ");
+        str = deleteSymbols(str, "\t");
+        int index = 0;
+        int commaChecker;
+        for (commaChecker = 0; index != -1; commaChecker++) {
+            index = str.indexOf(",");
+            if (commaChecker == 0) {
+                if (index == 0) {
+                    throw new IllegalArgumentException();
+                } else {
+                    name = str.substring(0, index);
+                }
+            }
+            if (commaChecker == 1) {
+                trSides[0] = Double.valueOf(str.substring(0, index));
+            }
+            if (commaChecker == 2) {
+                trSides[1] = Double.valueOf(str.substring(0, index));
+                trSides[2] = Double.valueOf(str.substring(index + 1, str.length()));
+            }
+            str = str.substring(index + 1, str.length());
+        }
+        if (commaChecker != 4) {
+            throw new IllegalArgumentException();
+        }
+        return new Triangle(name, trSides[0], trSides[1], trSides[2]);
+    }
 
-	private static boolean triangleSidesValidator(double a, double b, double c) {
-		if (a <= 0.0 || b <= 0.0 || c <= 0.0 || a + b > c || c + b > a || a + c > b) {
-			return false;
-		}
-		return true;
-	}
+    private static boolean triangleSidesValidator(double a, double b, double c) {
+        if (a <= 0.0 || b <= 0.0 || c <= 0.0 || a + b < c || c + b < a || a + c < b) {
+            return false;
+        }
+        return true;
+    }
 
-	private static String deleteSymbols(String str, String symb) {
-		StringBuilder temp = new StringBuilder(str);
-		int index = 0;
-		for (;;) {
-			index = temp.indexOf(symb);
-			if (index == -1) {
-				break;
-			} else {
-				temp = temp.deleteCharAt(index);
-			}
-		}
-		return temp.toString();
-	}
+    private static String deleteSymbols(String str, String symb) {
+        StringBuilder temp = new StringBuilder(str);
+        int index = 0;
+        for (;;) {
+            index = temp.indexOf(symb);
+            if (index == -1) {
+                break;
+            } else {
+                temp = temp.deleteCharAt(index);
+            }
+        }
+        return temp.toString();
+    }
 
-	private double calculateArea() {
-		double p = (sides[0] + sides[1] + sides[2]) / 2;
-		return Math.sqrt(p * (p - sides[0]) * (p - sides[1]) * (p - sides[2]));
-	}
+    private double calculateArea() {
+        double p = (sides[0] + sides[1] + sides[2]) / 2;
+        return Math.sqrt(p * (p - sides[0]) * (p - sides[1]) * (p - sides[2]));
+    }
 }
