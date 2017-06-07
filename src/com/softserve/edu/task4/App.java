616824a29da8b2467d8288d3d79e251d44d67d47
Crutovel <crutovel@gmail.com>
2017-06-07 16:57:02 +0300

    task 4 updated

--- src/com/softserve/edu/task4/App.java
+++ src/com/softserve/edu/task4/App.java
@@ -10,45 +10,49 @@ import java.util.Scanner;
 
 public class App {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		switch (args.length) {
-		case 2: {
-			int occurences = -1;
-			File file = new File(args[0]);
-			Scanner scanner;
-			try {
-				scanner = new Scanner(file);
+        switch (args.length) {
+            case 2: {
+                int occurences = 0;
+                File file = new File(args[0]);
+                Scanner scanner;
+                try {
+                    scanner = new Scanner(file);
 
-				scanner.useDelimiter(args[1]);
+                    scanner.useDelimiter(args[1]);
 
-				while (scanner.hasNext()) {
-					scanner.next();
-					occurences++;
-				}
-				scanner.close();
-			} catch (IOException e) {
-				System.out.println("IOException");
-			}
-			System.out.println("Occurences = " + occurences);
-			break;
-		}
-		case 3: {
-			Charset charset = Charset.forName("cp1251");
-			Path path = Paths.get(args[0]);
-			try {
-				Files.write(path,
-						new String(Files.readAllBytes(path), charset).replace(args[1], args[2]).getBytes(charset));
-			} catch (IOException e) {
-				System.out.println("IOException");
-			}
-			break;
-		}
-		default: {
-			System.out.println("There must be two or three parameters");
-		}
-		}
+                    while (scanner.hasNext()) {
+                        scanner.next();
+                        occurences++;
+                    }
+                    if (occurences != 0) {
+                        occurences--;
+                    }
+                    scanner.close();
+                } catch (IOException e) {
+                    System.out.println("Sry we have some problem. IOException");
+                    break;
+                }
+                System.out.println("Occurences = " + occurences);
+                break;
+            }
+            case 3: {
+                Charset charset = Charset.forName("cp1251");
+                Path path = Paths.get(args[0]);
+                try {
+                    Files.write(path,
+                            new String(Files.readAllBytes(path), charset).replace(args[1], args[2]).getBytes(charset));
+                } catch (IOException e) {
+                    System.out.println("Sry we have some problem. IOException");
+                }
+                break;
+            }
+            default: {
+                System.out.println("There must be two or three parameters");
+            }
+        }
 
-	}
+    }
 
 }
