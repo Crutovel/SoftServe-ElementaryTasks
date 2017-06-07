4a81bc06fec01b0aee2a5fa56f62984dfb4ca5f4
Crutovel <crutovel@gmail.com>
2017-06-07 16:59:13 +0300

    task 5 updated

--- src/com/softserve/edu/task5/App.java
+++ src/com/softserve/edu/task5/App.java
@@ -2,21 +2,21 @@ package com.softserve.edu.task5;
 
 public class App {
 
-	public static void main(String[] args) {
-		if (args.length == 1) {
-			BigNumber num = new BigNumber();
-			try {
-				num = new BigNumber(args[0]);
-				System.out.println(num.toString());
-			} catch (NumberFormatException e) {
-				System.out.println("Wrong Format");
-			} catch (IllegalArgumentException e) {
-				System.out.println("The number is too large");
-			}
-		}else{
-			System.out.println("Need one parameter");
-		}
+    public static void main(String[] args) {
+        if (args.length == 1) {
+            BigNumber num = new BigNumber();
+            try {
+                num = new BigNumber(args[0]);
+                System.out.println(num.toString());
+            } catch (NumberFormatException e) {
+                System.out.println("Wrong Format");
+            } catch (IllegalArgumentException e) {
+                System.out.println("The number is too large");
+            }
+        } else {
+            System.out.println("Need one parameter");
+        }
 
-	}
+    }
 
 }
