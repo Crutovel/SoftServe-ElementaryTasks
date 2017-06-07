82d6829969111ca1c2f63537a86fe750708fb5f9
Crutovel <crutovel@gmail.com>
2017-06-07 17:12:59 +0300

    task 8 updated

--- src/com/softserve/edu/task8/App.java
+++ src/com/softserve/edu/task8/App.java
@@ -2,37 +2,37 @@ package com.softserve.edu.task8;
 
 public class App {
 
-	public static void main(String[] args) {
-		Fibonacci nums = new Fibonacci();
-		switch (args.length) {
-		case 1: {
-			nums = new Fibonacci(getArg(args[0]));
-			break;
-		}
-		case 2: {
-			nums = new Fibonacci(getArg(args[0]), getArg(args[1]));
-			break;
-		}
-		default: {
-			System.out.println("There must be one or two parameters");
-		}
-		}
-		System.out.println(nums.toString());
-	}
+    public static void main(String[] args) {
+        Fibonacci nums = new Fibonacci();
+        switch (args.length) {
+            case 1: {
+                nums = new Fibonacci(getArg(args[0]));
+                break;
+            }
+            case 2: {
+                nums = new Fibonacci(getArg(args[0]), getArg(args[1]));
+                break;
+            }
+            default: {
+                System.out.println("There must be one or two parameters");
+            }
+        }
+        System.out.println(nums.toString());
+    }
 
-	private static int getArg(String arg) {
-		int result = 0;
-		try {
-			result = Integer.valueOf(arg);
-		} catch (NumberFormatException e) {
-			System.out.println("Wrong number format, need Integer value");
-			System.exit(0);
-		}
-		if (result <= 0) {
-			System.out.println("Parameters must be greater than zero and not greater than " + Integer.MAX_VALUE);
-			System.exit(0);
-		}
-		return result;
-	}
+    private static int getArg(String arg) {
+        int result = 0;
+        try {
+            result = Integer.valueOf(arg);
+        } catch (NumberFormatException e) {
+            System.out.println("Wrong number format, need Integer value");
+            System.exit(1);
+        }
+        if (result <= 0) {
+            System.out.println("Parameters must be greater than zero and not greater than " + Integer.MAX_VALUE);
+            System.exit(2);
+        }
+        return result;
+    }
 
 }
--- src/com/softserve/edu/task8/Fibonacci.java
+++ src/com/softserve/edu/task8/Fibonacci.java
@@ -1,71 +1,71 @@
 package com.softserve.edu.task8;
 
 import java.math.BigInteger;
-
 /**
  * 
  *I use the matrix formula to find N fibonacci number
  *F0=0; F1=1;
  *
  *P=|0 1|
- * 	|1 1|
+ *  |1 1|
  * 
  *(F0,F1)*P^n = (Fn Fn+1)
  */
 public class Fibonacci {
 
-	private BigInteger[] nums;
+    private BigInteger[] nums;
 
-	public Fibonacci() {
-		nums = new BigInteger[0];
-	}
+    public Fibonacci() {
+        nums = new BigInteger[0];
+    }
 
-	public Fibonacci(int n) {
-		if (n < 0) {
-			throw new IllegalArgumentException();
-		}
-		nums = new BigInteger[1];
-		nums[0] = Matrix.one.pow(n).get(0, 1);
-	}
+    public Fibonacci(int n) {
+        if (n < 0) {
+            throw new IllegalArgumentException();
+        }
+        nums = new BigInteger[1];
+        nums[0] = Matrix.one.pow(n).get(0, 1);
+    }
 
-	public Fibonacci(int begin, int end) {
-		if (end < begin || end < 0 || begin < 0) {
-			throw new IllegalArgumentException();
-		}
-		nums = new BigInteger[end - begin + 1];
-		Matrix temp = Matrix.one.pow(begin);
-		nums[0] = temp.get(0, 1);
-		if (begin != end) {
-			nums[1] = temp.get(1, 1);
+    public Fibonacci(int begin, int end) {
+        if (end < begin || end < 0 || begin < 0) {
+            throw new IllegalArgumentException();
+        }
+        nums = new BigInteger[end - begin + 1];
+        Matrix temp = Matrix.one.pow(begin);
+        nums[0] = temp.get(0, 1);
+        if (begin != end) {
+            nums[1] = temp.get(1, 1);
 
-			if (begin + 1 < end) {
-				for (int i = 2; i < end - begin + 1; i++) {
-					nums[i] = sumOfTwo(nums[i - 2], nums[i - 1]); 
-					//when i got 2 fib numbers i can continue to make fibonacci sequence by summing
-				}
-			}
-		}
-	}
+            if (begin + 1 < end) {
+                for (int i = 2; i < end - begin + 1; i++) {
+                    nums[i] = sumOfTwo(nums[i - 2], nums[i - 1]);
+                    // when i got 2 fib numbers i can continue to make fibonacci
+                    // sequence by summing
+                }
+            }
+        }
+    }
 
-	public BigInteger[] getNumbers() {
-		return nums;
-	}
+    public BigInteger[] getNumbers() {
+        return nums;
+    }
 
-	@Override
-	public String toString() {
-		if (nums.length == 0) {
-			return "";
-		}
-		StringBuilder result = new StringBuilder("");
-		for (int i = 0; i < nums.length - 1; i++) {
-			result.append(nums[i].toString() + ", ");
-		}
-		result.append(nums[nums.length - 1]);
-		return result.toString();
-	}
+    @Override
+    public String toString() {
+        if (nums.length == 0) {
+            return "";
+        }
+        StringBuilder result = new StringBuilder("");
+        for (int i = 0; i < nums.length - 1; i++) {
+            result.append(nums[i].toString() + ", ");
+        }
+        result.append(nums[nums.length - 1]);
+        return result.toString();
+    }
 
-	private BigInteger sumOfTwo(BigInteger a, BigInteger b) {
-		return a.add(b);
-	}
+    private BigInteger sumOfTwo(BigInteger a, BigInteger b) {
+        return a.add(b);
+    }
 
 }
